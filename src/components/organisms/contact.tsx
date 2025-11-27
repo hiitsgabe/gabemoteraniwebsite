a200454895c1212fda8c83abf2d477339a2e0e3a
gabe <gabe@gabes-MacBook-Air.local>
2025-11-27 18:56:44 -0500

    changed image

--- src/components/organisms/contact.tsx
+++ src/components/organisms/contact.tsx
@@ -22,13 +22,13 @@ function contact() {
   }, []);
 
   return (
-    <div className="dark w-full bg-background flex justify-center items-center py-20 relative">
+    <div className="dark w-full bg-background flex justify-center items-center py-20 pb-50 relative">
       <div className="pointer-events-none absolute inset-x-0 top-0 h-40 bg-linear-to-b from-background to-transparent z-20" />
 
       <ProfileCard
         name="Gabe Moterani"
         title="Software Engineer"
-        handle="gabemoterani"
+        handle="hiitsgabe"
         status="Online"
         contactText="Contact Me"
         avatarUrl="gabb.png"
